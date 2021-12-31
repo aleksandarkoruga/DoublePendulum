3f7d5878d4b191d10040337d9d13fca047b11905
unknown <aleksandar.koruga@gmail.com>
2021-12-31 18:40:02 +0100

    initial

--- plugins/DoublePendulum/DoublePendulum.cpp
+++ plugins/DoublePendulum/DoublePendulum.cpp
@@ -8,21 +8,67 @@ static InterfaceTable* ft;
 
 namespace DoublePendulum {
 
-DoublePendulum::DoublePendulum() {
+DoublePendulum::DoublePendulum() : pt1(),pt2(){
     mCalcFunc = make_calc_function<DoublePendulum, &DoublePendulum::next>();
     next(1);
 }
 
 void DoublePendulum::next(int nSamples) {
-    const float* input = in(0);
-    const float* gain = in(1);
-    float* outbuf = out(0);
 
-    // simple gain function
+    const float* m1 = in(0);
+    const float* m2 = in(1);
+    const float* l1 = in(2);
+    const float* l2 = in(3);
+    const float* k = in(4);
+    const float* t = in(5);
+    const float* freq = in(6);
+    const float* d = in(7);
+
+
+    float* outbuf = out(0);
+    float* outbuf1 = out(1);
+
+
     for (int i = 0; i < nSamples; ++i) {
-        outbuf[i] = input[i] * gain[i];
+
+        pt1.mass = m1[i];
+        pt2.mass = m2[i];
+        pt1.length = l1[i];
+        pt2.length = l2[i];
+
+        UpdateSystem(t[i], d[i], freq[i], k[i]);
+        
+        outbuf[i] = pt1.phi;
+        outbuf1[i] = pt1.theta;
+
     }
 }
+
+void DoublePendulum::TimeStep(float dT, float damp)
+{
+    float dA = pt1.phi - pt1.theta;
+
+    //pt2
+    float x = -1.0f * (pt2.mass * ((pt2.length * powf( pt2.theta,2.0f)  ) + (pt1.length *(float) powf(pt2.phi,2.0f)  * cosf(dA))) * sinf(dA)) / (pt1.length * (pt1.mass + (pt2.mass * (float) powf(sinf(dA),2.0f)    )));
+    //pt2
+    float y= (((pt1.mass + pt2.mass) * (pt1.length * powf(pt2.phi,2.0f) ) + pt2.length * pt2.mass * cosf(dA) * powf(pt2.theta,2.0f) ) * (sinf(dA))) / (pt2.length * (pt1.mass + (pt2.mass * powf(sinf(dA), 2.0f))   ));
+   
+    
+    pt2.phi = zapgremlins( sc_wrap2((pt2.phi+(x*dT))*damp, twopi_f));
+    pt2.theta =zapgremlins( sc_wrap2((pt2.theta + (y * dT)) * damp, twopi_f));
+
+    pt1.phi = zapgremlins(sc_wrap2(pt1.phi + pt2.phi*dT, twopi_f));
+    pt1.theta = zapgremlins(sc_wrap2(pt1.theta + pt2.theta * dT, twopi_f));
+
+}
+void DoublePendulum::UpdateSystem(float dT, float damp, float forcingFreq, float forcingConst )
+{
+    pt1.theta += forcingConst * sinf(rot * forcingFreq);
+    pt1.theta= zapgremlins(sc_wrap2(pt1.theta, twopi_f));
+    TimeStep(dT, damp);
+    rot = sc_mod(pt1.theta, twopi_f);
+}
+
 
 } // namespace DoublePendulum
 
--- plugins/DoublePendulum/DoublePendulum.hpp
+++ plugins/DoublePendulum/DoublePendulum.hpp
@@ -6,6 +6,15 @@
 #include "SC_PlugIn.hpp"
 
 namespace DoublePendulum {
+    struct Component 
+    {
+        float phi;
+        float theta;
+        float mass;
+        float length;
+        Component() : phi( std::rand()/(float) RAND_MAX), theta(std::rand() / (float)RAND_MAX), mass(1), length(1) {}
+    };
+
 
 class DoublePendulum : public SCUnit {
 public:
@@ -19,6 +28,18 @@ private:
     void next(int nSamples);
 
     // Member variables
+protected:
+
+    Component pt1;
+    Component pt2;
+    float rot;
+
+    void TimeStep(float dT,float damp);
+
+    void UpdateSystem(float dT, float damp, float forcingFreq, float forcingConst);
+    
+
+
 };
 
 } // namespace DoublePendulum
